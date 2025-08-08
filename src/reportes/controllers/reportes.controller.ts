ab6a2bb0857bfc05bbcf9db87972fe96dee52d64
Jos3MA56 <20230087@uthh.edu.mx>
2025-08-07 21:16:23 -0600

    Actualizaci├│n autom├ítica - 07/08/2025 09:16

--- src/reportes/controllers/reportes.controller.ts
+++ src/reportes/controllers/reportes.controller.ts
@@ -16,4 +16,22 @@ export class ReportesController {
   async obtenerMateriasDeAlumno(@Param('id') id: string) {
     return this.reportesService.reporteMateriasPorAlumno(id);
   }
+
+  @ApiOperation({ summary: 'Obtener un alumno con su o sus materias' })
+  @ApiParam({ name: 'id', required: true, description: 'ID del alumno' })
+  @ApiResponse({ status: 400, description: 'ID de alumno inválido' })
+  @ApiResponse({ status: 200, description: 'Datos obtenidos correctamente' })
+  @ApiResponse({ status: 404, description: 'Datos no encontrado' })
+  async obtenerMateriasPorDocente(@Query('docenteId') docenteId: string) {
+    return this.reportesService.reporteMateriasPorDocente(docenteId);
+  }
+
+  @ApiOperation({ summary: 'Obtener un alumno con su o sus materias' })
+  @ApiParam({ name: 'id', required: true, description: 'ID de la materia' })
+  @ApiResponse({ status: 400, description: 'ID de materia inválido' })
+  @ApiResponse({ status: 200, description: 'Datos obtenidos correctamente' })
+  @ApiResponse({ status: 404, description: 'Datos no encontrado' })
+  async obtenerAlumnosPorMateria(@Query('materiaId') materiaId: string) {
+    return this.reportesService.reporteAlumnosPorMateria(materiaId);
+  }
 }
