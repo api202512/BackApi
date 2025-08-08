150ecc66a5e30f68e03f0cf8aafa2c86709a9dca
Jos3MA56 <20230087@uthh.edu.mx>
2025-08-07 18:47:42 -0600

    Update reportes.controller.ts

--- src/reportes/controllers/reportes.controller.ts
+++ src/reportes/controllers/reportes.controller.ts
@@ -7,21 +7,6 @@ import { ReportesService } from './../services/reportes.service';
 export class ReportesController {
   constructor(private readonly reportesService: ReportesService) {}
 
-  @Get('alumnos')
-  obtenerAlumnosInscritos() {
-    return this.reportesService.reporteAlumnos();
-  }
-
-  @Get('materias')
-  obtenerMateriasPorCiclo(@Query('cicloId') cicloId: string) {
-    return this.reportesService.reporteMateriasPorCiclo(cicloId);
-  }
-
-  @Get('docentes')
-  obtenerDocentesAsignados() {
-    return this.reportesService.reporteDocentes();
-  }
-
   @ApiOperation({ summary: 'Obtener un alumno con su o sus materias' })
   @ApiParam({ name: 'id', required: true, description: 'ID del alumno' })
   @ApiResponse({ status: 400, description: 'ID de alumno inválido' })
