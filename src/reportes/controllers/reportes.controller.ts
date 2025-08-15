53e9860a0729775e002a22038f2361d25c1f1c3f
Jos3MA56 <20230087@uthh.edu.mx>
2025-08-15 14:10:24 -0600

    Se agregó una funcion en reportes.controller - 15/08/2025 02:09

--- src/reportes/controllers/reportes.controller.ts
+++ src/reportes/controllers/reportes.controller.ts
@@ -6,4 +6,14 @@ import { ReportesService } from './../services/reportes.service';
 @Controller('reportes')
 export class ReportesController {
   constructor(private readonly reportesService: ReportesService) {}
+
+  @ApiOperation({ summary: 'Obtener un alumno con su o sus materias' })
+  @ApiParam({ name: 'id', required: true, description: 'ID del alumno' })
+  @ApiResponse({ status: 400, description: 'ID de alumno inválido' })
+  @ApiResponse({ status: 200, description: 'Datos obtenidos correctamente' })
+  @ApiResponse({ status: 404, description: 'Datos no encontrado' })
+  @Get('alumnos/:id/materias')
+  async obtenerMateriasDeAlumno(@Param('id') id: string) {
+    return this.reportesService.reporteMateriasPorAlumno(id);
+  }
 }
